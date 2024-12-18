688a8281cc2944b4046b50aa69133ea6814fbb33
hyuk2 <hyuk2000s@gmail.com>
2024-12-18 18:03:01 +0900

    first commit

---
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -0,0 +1,54 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Aspect
+@Slf4j
+public class AspectV6Advice {
+
+
+/*   // hello.aop.order 패키지와 하위 패키지 이면서 클래스 이름 패턴이 *Service
+    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            //@Before
+            log.info("[transaction] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            //@AfterReturning
+            log.info("[transaction commit] {}", joinPoint.getSignature());
+            return result;
+        }catch (Exception e) {
+            //@AfterThrowing
+            log.info("[transaction rollback] {}", joinPoint.getSignature());
+            throw e;
+        }finally {
+            //@After
+            log.info ("[resource release] {}", joinPoint.getSignature());
+        }
+    }
+*/
+
+
+    @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[return] {} return {}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
+    public void doAfterThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+
+    @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
---
+++ src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,45 @@
+package hello.aop;
+
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@SpringBootTest
+@Slf4j
+public class AopTest {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("itemaA");
+    }
+
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(()-> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
